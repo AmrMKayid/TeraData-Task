f921c17086ce757380971a7c5a6a085a40881535
Amr M. Kayid <amrkayid2027@gmail.com>
2018-03-09 23:02:32 +0200

    Modify Task 1

--- src/Task1_Read2Lists.java
+++ src/Task1_Read2Lists.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class Task1_Read2Lists {
 
@@ -10,32 +9,17 @@ public class Task1_Read2Lists {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("resources/task1_lists.csv"));
-        String line =  null;
-        HashMap<String, String> map = new HashMap<String, String>();
+        String line, s1 = "", s2 = "";
 
-        while((line=br.readLine())!=null){
+        while((line = br.readLine())!=null){
             String str[] = line.split(",");
-            map.put(str[0], str[1]);
+            s1 += str[0] + " ";
+            s2 += str[1] + " ";
         }
 
-//        System.out.println(map);
+        System.out.println("L1 = " + Arrays.toString(s1.split(" ")));
+        System.out.println("L2 = " + Arrays.toString(s2.split(" ")));
 
-        String l1[] = map.keySet().toArray(new String[0]);
-        l1 =  Arrays.stream(l1)
-                .filter(value ->
-                        value != null && value.length() > 0
-                )
-                .toArray(size -> new String[size]);
-
-        System.out.println("L1 = " + Arrays.toString(l1));
-
-        String l2[] = map.values().toArray(new String[0]);
-        l2 =  Arrays.stream(l2)
-                .filter(value ->
-                        value != null && value.length() > 0
-                )
-                .toArray(size -> new String[size]);
-        System.out.println("L2 = " + Arrays.toString(l2));
     }
 
 
