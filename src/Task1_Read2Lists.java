a3432651b9f29b43d728b57d30a9643b2af32bea
Amr M. Kayid <amrkayid2027@gmail.com>
2018-03-14 16:02:25 +0200

    Finish Task

--- src/Task1_Read2Lists.java
+++ src/Task1_Read2Lists.java
@@ -2,16 +2,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Task1_Read2Lists {
 
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new FileReader("resources/task1_lists.csv"));
+        String path = "resources/task1_lists.csv";
+
+        System.out.println("Method 1: ");
+        Method1_UsingStrings(path);
+
+        System.out.println("###################################");
+
+        System.out.println("Method 2: ");
+        Method2_UsingHashMap(path);
+
+    }
+
+    private static void Method1_UsingStrings(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
         String line, s1 = "", s2 = "";
 
-        while((line = br.readLine())!=null){
+        while ((line = br.readLine()) != null) {
             String str[] = line.split(",");
             s1 += str[0] + " ";
             s2 += str[1] + " ";
@@ -19,7 +33,37 @@ public class Task1_Read2Lists {
 
         System.out.println("L1 = " + Arrays.toString(s1.split(" ")));
         System.out.println("L2 = " + Arrays.toString(s2.split(" ")));
+    }
 
+    private static void Method2_UsingHashMap(String path) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        String line;
+        HashMap<String, String> map = new HashMap<>();
+
+        while ((line = br.readLine()) != null) {
+            String str[] = line.split(",");
+            map.put(str[0], str[1]);
+        }
+
+//        System.out.println(map);
+
+        String l1[] = map.keySet().toArray(new String[0]);
+        l1 = Arrays.stream(l1)
+                .filter(value ->
+                        value != null && value.length() > 0
+                )
+                .toArray(String[]::new);
+
+        System.out.println("L1 = " + Arrays.toString(l1));
+
+        String l2[] = map.values().toArray(new String[0]);
+        l2 = Arrays.stream(l2)
+                .filter(value ->
+                        value != null && value.length() > 0
+                )
+                .toArray(String[]::new);
+        System.out.println("L2 = " + Arrays.toString(l2));
     }
 
 
--- src/Task2_DraggableTable.java
+++ src/Task2_DraggableTable.java
@@ -1,40 +1,145 @@
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.DefaultTableModel;
 
-public class Task2_DraggableTable extends JFrame {
+public class Task2_DraggableTable {
+    public Task2_DraggableTable(String t1[], String t2[]) {
+        final MobileContainerPanel mcp = new MobileContainerPanel();
 
-    private String[] t1;
-    private String[] t2;
+        DefaultTableModel model = new DefaultTableModel();
+        JTable table = new JTable(model);
+        for (String s : t1)
+            model.addColumn(s);
 
-    public Task2_DraggableTable(String[] t1, String[] t2) {
+        table.setBorder(BorderFactory.createEtchedBorder());
+        mcp.addNext(new JScrollPane(table));
 
-        this.t1 = t1; this.t2 = t2;
+        DefaultTableModel model2 = new DefaultTableModel();
+        JTable table2 = new JTable(model2);
+        for (String s : t2)
+            model2.addColumn(s);
 
-        this.setTitle("Task2: Draggable Table");
-        this.setDefaultLookAndFeelDecorated(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(2, 0));
+        table2.setBorder(BorderFactory.createEtchedBorder());
+        mcp.addNext(new JScrollPane(table2));
 
-        Object[][] data = new Object[0][];
-        JTable table = new JTable(data, t1);
-
-        this.add(new JScrollPane(table));
-
-        JTable table2 = new JTable(data, t2);
-        this.add(new JScrollPane(table2));
-
-        this.pack();
-        this.setVisible(true);
+        JPanel north = new JPanel();
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(north, "North");
+        f.getContentPane().add(mcp);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        f.setSize((int) screenSize.getWidth(), (int) screenSize.getHeight());
+        f.setLocation(200, 200);
+        f.setVisible(true);
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
+        new Task2_DraggableTable(new String[]{"one", "two", "three"}, new String[]{"one", "three", "two"});
+    }
+}
 
-        String s1[] = {"one", "two", "three"};
-        String s2[] = {"one", "three", "two"};
+class MobileContainerPanel extends JPanel {
+    final int PAD = 10;
+    List componentList;
+    ComponentWrangler wrangler;
 
-        new Task2_DraggableTable(s1, s2);
+    public MobileContainerPanel() {
+        componentList = new ArrayList();
+        wrangler = new ComponentWrangler();
+        setLayout(null);
     }
 
+    public void addNext(Component c) {
+        componentList.add(c);
+        c.addMouseListener(wrangler);
+        c.addMouseMotionListener(wrangler);
+        add(c);
+        Dimension d = c.getPreferredSize();
+        Point p = getNextLocation(d);
+        c.setBounds(p.x, p.y, d.width, d.height);
+        repaint();
+    }
 
+    private Point getNextLocation(Dimension d) {
+        int maxX = 0, maxY = 0;
+        Component c, last = null;
+        Rectangle r;
+        // find level of lowest component(s)
+        for (int j = 0; j < componentList.size(); j++) {
+            c = (Component) componentList.get(j);
+            r = c.getBounds();
+            if (r.y + r.height > maxY) {
+                maxY = r.y + r.height;
+                last = c;
+            }
+        }
+        // find last (in row) of lowest components
+        for (int j = 0; j < componentList.size(); j++) {
+            c = (Component) componentList.get(j);
+            r = c.getBounds();
+            if (r.y + r.height == maxY && r.x + r.width > maxX) {
+                maxX = r.x + r.width;
+                last = c;
+            }
+        }
+        // determine location of next component based on location of last
+        Point p = new Point();
+        if (last == null) {
+            p.x = PAD;
+            p.y = PAD;
+            return p;
+        }
+        r = last.getBounds();
+        int x, y;
+        if (r.x + r.width + PAD + d.width < getWidth()) {
+            p.x = r.x + r.width + PAD;
+            p.y = r.y;
+        } else {
+            p.x = PAD;
+            p.y = r.y + r.height + PAD;
+        }
+        return p;
+    }
 
+    public void clear() {
+        removeAll();
+        componentList.clear();
+        repaint();
+    }
+
+    /**
+     * select and drag components with the mouse
+     */
+    private class ComponentWrangler extends MouseInputAdapter {
+        Component selectedComponent;
+        Point offset;
+        boolean dragging;
+
+        public ComponentWrangler() {
+            dragging = false;
+        }
+
+        public void mousePressed(MouseEvent e) {
+            selectedComponent = (Component) e.getSource();
+            offset = e.getPoint();
+            dragging = true;
+        }
+
+        public void mouseReleased(MouseEvent e) {
+            dragging = false;
+        }
+
+        public void mouseDragged(MouseEvent e) {
+            if (dragging) {
+                Rectangle r = selectedComponent.getBounds();
+                r.x += e.getX() - offset.x;
+                r.y += e.getY() - offset.y;
+                selectedComponent.setBounds(r);
+            }
+        }
+    }
 }
